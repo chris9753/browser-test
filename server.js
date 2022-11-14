73b5f49ac334f49283c3eb8b6edfcabbeb48e212
Christopher Da Silva <mr.chrisdasilver@gmail.com>
2022-11-14 15:03:23 -0500

    h@xor protection

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const users = {
 
 // sendFile will go here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/index_goodluck.html'));
 });
 
 app.post('/verify', function(req, res) {
